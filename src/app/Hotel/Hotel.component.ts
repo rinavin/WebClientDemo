a4e70ed0b377f0be43f2d104517a69c2f2b00701
Rina Vinokurov <rina_vinokurov@magicsoftware.com>
2017-12-07 14:43:12 +0200

    changes for stub creation

--- src/app/Hotel/Hotel.component.ts
+++ src/app/Hotel/Hotel.component.ts
@@ -8,4 +8,9 @@ import { TaskMagicService } from "../magic/src/services/task.magics.service";
 	styleUrls: ["./Hotel.component.css"],
 	templateUrl: "./Hotel.component.html"
 })
-export class Hotel extends BaseTaskMagicComponent {}
+export class Hotel extends BaseTaskMagicComponent {
+
+  loadData():any   {
+    let stubData = {"records":{"data":{"0":{"values":{"image1":"assets/images/Travel/","image2":"assets/images/Travel/","image3":"assets/images/Travel/","image4":"assets/images/Travel/"},"ControlsProperties":{"image1":{"properties":{"image":"assets/images/Travel/","tabindex":"25"},"classesMap":{},"stylesMap":{}},"image2":{"properties":{"image":"assets/images/Travel/","tabindex":"26"},"classesMap":{},"stylesMap":{}},"image3":{"properties":{"image":"assets/images/Travel/","tabindex":"27"},"classesMap":{},"stylesMap":{}},"image4":{"properties":{"image":"assets/images/Travel/","tabindex":"28"},"classesMap":{},"stylesMap":{}}},"rowId":"0"}},"list":[{"values":{"image1":"assets/images/Travel/","image2":"assets/images/Travel/","image3":"assets/images/Travel/","image4":"assets/images/Travel/"},"ControlsProperties":{"image1":{"properties":{"image":"assets/images/Travel/","tabindex":"25"},"classesMap":{},"stylesMap":{}},"image2":{"properties":{"image":"assets/images/Travel/","tabindex":"26"},"classesMap":{},"stylesMap":{}},"image3":{"properties":{"image":"assets/images/Travel/","tabindex":"27"},"classesMap":{},"stylesMap":{}},"image4":{"properties":{"image":"assets/images/Travel/","tabindex":"28"},"classesMap":{},"stylesMap":{}}},"rowId":"0"}]},"template":{}};
+    this.loadStubData(stubData);}
+}
