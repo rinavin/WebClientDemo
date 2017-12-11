b190e7a222ab35c112ebde1389433f017ab23940
Rina Vinokurov <rina_vinokurov@magicsoftware.com>
2017-12-11 10:54:02 +0200

    change stub data

--- src/app/Flight1/Flight1.component.ts
+++ src/app/Flight1/Flight1.component.ts
@@ -17,24 +17,25 @@ export class Flight1 extends BaseTaskMagicComponent {
           "0": {
             "values": {
               "AddFlightImg": "assets/images/Travel/flight.png",
-              "fromairportEdit": "This Airport Edit",
-              "airportNameFrom": "This Airport namefrom",
-              "toairportEdit": "JOR",
-              "airportNameTo": "The City Heliport",
-              "fromdateEdit": "01-Jan-1901",
+              "fromairportEdit": "",
+              "airportNameFrom": "",
+              "toairportEdit": "BKK",
+              "airportNameTo": "Suvarnabhumi International",
+              "fromdateEdit": "15-Jan-2012",
               "fromdateTimeEdit": "00:00",
-              "todateEdit": "01-Jan-1901",
+              "todateEdit": "16-Jan-2012",
               "todateTimeEdit": "00:00",
-              "flightnumberEdit": "432",
-              "classImg": "assets/images/Travel/airline-TG.gif",
-              "airline": "4",
+              "flightnumberEdit": "454",
+              "classImg": "assets/images/Travel/airline-LH.gif",
+              "airline": "1",
               "BSave": "Ok"
             },
             "ControlsProperties": {
               "AddFlightImg": {
-                "properties": {"image": "assets/images/Travel/flight.png", "tabindex": "18"},
-                "classesMap": {},
-                "stylesMap": {}
+                "properties": {
+                  "image": "assets/images/Travel/flight.png",
+                  "tabindex": "19"
+                }, "classesMap": {}, "stylesMap": {}
               },
               "SelectFlightL": {"properties": {"text": "Flight Details"}, "classesMap": {}, "stylesMap": {}},
               "OriginL": {"properties": {"text": "Origin:"}, "classesMap": {}, "stylesMap": {}},
@@ -44,7 +45,7 @@ export class Flight1 extends BaseTaskMagicComponent {
               "SearchSubform": {"properties": {"visible": "0"}, "classesMap": {}, "stylesMap": {}},
               "L_Flight": {"properties": {"text": "Flight:"}, "classesMap": {}, "stylesMap": {}},
               "classImg": {
-                "properties": {"image": "assets/images/Travel/airline-TG.gif", "tabindex": "29"},
+                "properties": {"image": "assets/images/Travel/airline-LH.gif", "tabindex": "30"},
                 "classesMap": {},
                 "stylesMap": {}
               },
@@ -56,19 +57,19 @@ export class Flight1 extends BaseTaskMagicComponent {
                   }, {"index": "2", "realString": "JL"}, {"index": "3", "realString": "QF"}, {
                     "index": "4",
                     "realString": "TG"
-                  }, {"index": "5", "realString": "AA"}], "selectedvalue": "4", "tabindex": "27"
+                  }, {"index": "5", "realString": "AA"}], "selectedvalue": "1", "tabindex": "28"
                 }, "classesMap": {}, "stylesMap": {}
               },
-              "BSave": {"properties": {"enabled": "0", "tabindex": "30"}, "classesMap": {}, "stylesMap": {}},
-              "fromairportEdit": {"properties": {"tabindex": "19"}, "classesMap": {}, "stylesMap": {}},
-              "airportNameFrom": {"properties": {"tabindex": "20"}, "classesMap": {}, "stylesMap": {}},
-              "toairportEdit": {"properties": {"tabindex": "21"}, "classesMap": {}, "stylesMap": {}},
-              "airportNameTo": {"properties": {"tabindex": "22"}, "classesMap": {}, "stylesMap": {}},
-              "fromdateEdit": {"properties": {"tabindex": "23"}, "classesMap": {}, "stylesMap": {}},
-              "fromdateTimeEdit": {"properties": {"tabindex": "24"}, "classesMap": {}, "stylesMap": {}},
-              "todateEdit": {"properties": {"tabindex": "25"}, "classesMap": {}, "stylesMap": {}},
-              "todateTimeEdit": {"properties": {"tabindex": "26"}, "classesMap": {}, "stylesMap": {}},
-              "flightnumberEdit": {"properties": {"tabindex": "28"}, "classesMap": {}, "stylesMap": {}}
+              "BSave": {"properties": {"enabled": "0", "tabindex": "31"}, "classesMap": {}, "stylesMap": {}},
+              "fromairportEdit": {"properties": {"tabindex": "20"}, "classesMap": {}, "stylesMap": {}},
+              "airportNameFrom": {"properties": {"tabindex": "21"}, "classesMap": {}, "stylesMap": {}},
+              "toairportEdit": {"properties": {"tabindex": "22"}, "classesMap": {}, "stylesMap": {}},
+              "airportNameTo": {"properties": {"tabindex": "23"}, "classesMap": {}, "stylesMap": {}},
+              "fromdateEdit": {"properties": {"tabindex": "24"}, "classesMap": {}, "stylesMap": {}},
+              "fromdateTimeEdit": {"properties": {"tabindex": "25"}, "classesMap": {}, "stylesMap": {}},
+              "todateEdit": {"properties": {"tabindex": "26"}, "classesMap": {}, "stylesMap": {}},
+              "todateTimeEdit": {"properties": {"tabindex": "27"}, "classesMap": {}, "stylesMap": {}},
+              "flightnumberEdit": {"properties": {"tabindex": "29"}, "classesMap": {}, "stylesMap": {}}
             },
             "rowId": "0"
           }
@@ -76,24 +77,25 @@ export class Flight1 extends BaseTaskMagicComponent {
         "list": [{
           "values": {
             "AddFlightImg": "assets/images/Travel/flight.png",
-            "fromairportEdit": "This Airport Edit 2",
-            "airportNameFrom": "This Airport namefrom 2",
-            "toairportEdit": "JOR",
-            "airportNameTo": "The City Heliport",
-            "fromdateEdit": "01-Jan-1901",
+            "fromairportEdit": "",
+            "airportNameFrom": "",
+            "toairportEdit": "BKK",
+            "airportNameTo": "Suvarnabhumi International",
+            "fromdateEdit": "15-Jan-2012",
             "fromdateTimeEdit": "00:00",
-            "todateEdit": "01-Jan-1901",
+            "todateEdit": "16-Jan-2012",
             "todateTimeEdit": "00:00",
-            "flightnumberEdit": "747",
-            "classImg": "assets/images/Travel/airline-TG.gif",
-            "airline": "4",
+            "flightnumberEdit": "454",
+            "classImg": "assets/images/Travel/airline-LH.gif",
+            "airline": "1",
             "BSave": "Ok"
           },
           "ControlsProperties": {
             "AddFlightImg": {
-              "properties": {"image": "assets/images/Travel/flight.png", "tabindex": "18"},
-              "classesMap": {},
-              "stylesMap": {}
+              "properties": {
+                "image": "assets/images/Travel/flight.png",
+                "tabindex": "19"
+              }, "classesMap": {}, "stylesMap": {}
             },
             "SelectFlightL": {"properties": {"text": "Flight Details"}, "classesMap": {}, "stylesMap": {}},
             "OriginL": {"properties": {"text": "Origin:"}, "classesMap": {}, "stylesMap": {}},
@@ -103,30 +105,31 @@ export class Flight1 extends BaseTaskMagicComponent {
             "SearchSubform": {"properties": {"visible": "0"}, "classesMap": {}, "stylesMap": {}},
             "L_Flight": {"properties": {"text": "Flight:"}, "classesMap": {}, "stylesMap": {}},
             "classImg": {
-              "properties": {"image": "assets/images/Travel/airline-TG.gif", "tabindex": "29"},
+              "properties": {"image": "assets/images/Travel/airline-LH.gif", "tabindex": "30"},
               "classesMap": {},
               "stylesMap": {}
             },
             "airline": {
               "properties": {
-                "itemslist": [{"index": "0", "realString": "DL"}, {"index": "1", "realString": "LH"}, {
-                  "index": "2",
-                  "realString": "JL"
-                }, {"index": "3", "realString": "QF"}, {"index": "4", "realString": "TG"}, {"index": "5", "realString": "AA"}],
-                "selectedvalue": "4",
-                "tabindex": "27"
+                "itemslist": [{"index": "0", "realString": "DL"}, {
+                  "index": "1",
+                  "realString": "LH"
+                }, {"index": "2", "realString": "JL"}, {"index": "3", "realString": "QF"}, {
+                  "index": "4",
+                  "realString": "TG"
+                }, {"index": "5", "realString": "AA"}], "selectedvalue": "1", "tabindex": "28"
               }, "classesMap": {}, "stylesMap": {}
             },
-            "BSave": {"properties": {"enabled": "0", "tabindex": "30"}, "classesMap": {}, "stylesMap": {}},
-            "fromairportEdit": {"properties": {"tabindex": "19"}, "classesMap": {}, "stylesMap": {}},
-            "airportNameFrom": {"properties": {"tabindex": "20"}, "classesMap": {}, "stylesMap": {}},
-            "toairportEdit": {"properties": {"tabindex": "21"}, "classesMap": {}, "stylesMap": {}},
-            "airportNameTo": {"properties": {"tabindex": "22"}, "classesMap": {}, "stylesMap": {}},
-            "fromdateEdit": {"properties": {"tabindex": "23"}, "classesMap": {}, "stylesMap": {}},
-            "fromdateTimeEdit": {"properties": {"tabindex": "24"}, "classesMap": {}, "stylesMap": {}},
-            "todateEdit": {"properties": {"tabindex": "25"}, "classesMap": {}, "stylesMap": {}},
-            "todateTimeEdit": {"properties": {"tabindex": "26"}, "classesMap": {}, "stylesMap": {}},
-            "flightnumberEdit": {"properties": {"tabindex": "28"}, "classesMap": {}, "stylesMap": {}}
+            "BSave": {"properties": {"enabled": "0", "tabindex": "31"}, "classesMap": {}, "stylesMap": {}},
+            "fromairportEdit": {"properties": {"tabindex": "20"}, "classesMap": {}, "stylesMap": {}},
+            "airportNameFrom": {"properties": {"tabindex": "21"}, "classesMap": {}, "stylesMap": {}},
+            "toairportEdit": {"properties": {"tabindex": "22"}, "classesMap": {}, "stylesMap": {}},
+            "airportNameTo": {"properties": {"tabindex": "23"}, "classesMap": {}, "stylesMap": {}},
+            "fromdateEdit": {"properties": {"tabindex": "24"}, "classesMap": {}, "stylesMap": {}},
+            "fromdateTimeEdit": {"properties": {"tabindex": "25"}, "classesMap": {}, "stylesMap": {}},
+            "todateEdit": {"properties": {"tabindex": "26"}, "classesMap": {}, "stylesMap": {}},
+            "todateTimeEdit": {"properties": {"tabindex": "27"}, "classesMap": {}, "stylesMap": {}},
+            "flightnumberEdit": {"properties": {"tabindex": "29"}, "classesMap": {}, "stylesMap": {}}
           },
           "rowId": "0"
         }]
@@ -146,6 +149,5 @@ export class Flight1 extends BaseTaskMagicComponent {
       }
     };
     this.loadStubData(stubData);
-
   }
 }
